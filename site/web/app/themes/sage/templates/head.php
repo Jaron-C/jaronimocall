52caaf9c9ec91684dc22f6cf6c6d06cefa856f2a
Jaron Call <jaroninthesis@gmail.com>
2016-03-04 22:42:54 -0700

    about to update gulpfile

--- site/web/app/themes/sage/templates/head.php
+++ site/web/app/themes/sage/templates/head.php
@@ -3,4 +3,5 @@
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <?php wp_head(); ?>
+  <link rel="stylesheet" type="text/css" href="https://www.google.com/fonts#UsePlace:use/Collection:Roboto:400,100,300">
 </head>
