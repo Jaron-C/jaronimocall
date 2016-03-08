6e3e95e8e5836e371c230634e10b80a2b0fa0a02
Jaron Call <jaroninthesis@gmail.com>
2016-03-08 00:42:49 -0700

    Organized svgs

--- site/web/app/themes/sage/templates/hero.php
+++ site/web/app/themes/sage/templates/hero.php
@@ -1,10 +1,14 @@
 <div class="container" id="hero">
-  <div>
-    <svg><?php get_template_part( 'assets/svg/inline', 'hair-logo.svg' ); ?></svg>
+  <div class="text-center">
+    <svg class="hair-logo" width="321" height="426"><?php get_template_part( 'assets/svg/inline', 'hair-logo.svg' ); ?></svg>
   </div>
 </div>
 
-<h1>Jaronimo Call</h1>
-<h3>Motion Graphics | Website Design</h3>
+<div class="text-center">
+  <h1>Jaronimo Call</h1>
+  <h3>Motion Graphics | Website Design</h3>
+</div>
 
-<svg><?php get_template_part( 'assets/svg/inline', 'findme-logos.svg' ); ?></svg>
+<div class="my-brands text-center">
+<svg class="brand-logos" width="340" height="100"><?php get_template_part( 'assets/svg/inline', 'findme-logos.svg' ); ?></svg>
+</div>
