a602b55fc608be8300c9e08f7705477cf0461760
Jaron Call <jaroninthesis@gmail.com>
2016-03-08 13:43:25 -0700

    Fixed syntax/changed font color

--- site/web/app/themes/sage/templates/section1.php
+++ site/web/app/themes/sage/templates/section1.php
@@ -1,5 +1,5 @@
 <div id="section1">
   <div class="text-center">
-    <svg class="hair-logo" width="421" height="526"><?php get_template_part( 'assets/svg/inline', 'companies.svg' ); ?></svg>
+    <svg class="companies" width="421" height="526"><?php get_template_part( 'assets/svg/inline', 'companies.svg' ); ?></svg>
   </div>
 </div>
