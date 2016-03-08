c4f60abffbab3bf4d382f10ffee52594ad7e93b9
Jaron Call <jaroninthesis@gmail.com>
2016-03-08 10:25:50 -0700

    Created id Section1

--- site/web/app/themes/sage/templates/hero.php
+++ site/web/app/themes/sage/templates/hero.php
@@ -1,14 +1,10 @@
 <div class="container" id="hero">
   <div class="text-center">
-    <svg class="hair-logo" width="321" height="426"><?php get_template_part( 'assets/svg/inline', 'hair-logo.svg' ); ?></svg>
+    <svg class="hair-logo" width="421" height="526"><?php get_template_part( 'assets/svg/inline', 'hair-logo.svg' ); ?></svg>
   </div>
 </div>
 
-<div class="text-center">
+<div class="name text-center">
   <h1>Jaronimo Call</h1>
   <h3>Motion Graphics | Website Design</h3>
-</div>
-
-<div class="my-brands text-center">
-<svg width="340" height="100"><?php get_template_part( 'assets/svg/inline', 'findme-logos.svg' ); ?></svg>
 </div>
--- site/web/app/themes/sage/templates/section1.php
+++ site/web/app/themes/sage/templates/section1.php
@@ -1,18 +1,5 @@
-<!--<h3>Motion Graphics | Website Design</h3>--!>
-
-<!-- Featured Category -->
-<!--<div class="wrap container">
-<div class="row">
-<?php query_posts('category_name=featured&showposts=3'); while (have_posts()) : the_post();?>
-<div class="col-sm-4 col-xs-12">
-<div class="thumbnail">
-<?php the_post_thumbnail( 'featured-img-thumb' ); ?>
-<h3><a href="<?php the_permalink() ?>" title="Permanent Link to <?php the_title(); ?>"><?php the_title(); ?></a></h3>
-<?php get_template_part('templates/entry-meta'); ?>
-<?php the_excerpt(); ?>
+<div id="section1">
+  <div class="brands text-center">
+<svg width="340" height="100"><?php get_template_part( 'assets/svg/inline', 'findme-logos.svg' ); ?></svg>
+  </div>
 </div>
-</div>
-<?php endwhile; ?>
-</div>
-</div>--!>
-<!-- //Featured Category -->
