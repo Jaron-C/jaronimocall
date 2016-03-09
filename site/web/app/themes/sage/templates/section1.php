0d9945ee24dfaffd79ad223ea71c840be4886a8a
Jaron Call <jaroninthesis@gmail.com>
2016-03-08 21:42:17 -0700

    Fixed size of "section1"

--- site/web/app/themes/sage/templates/section1.php
+++ site/web/app/themes/sage/templates/section1.php
@@ -1,5 +1,5 @@
 <div id="section1">
   <div class="text-center">
-    <svg class="companies" width="421" height="526"><?php get_template_part( 'assets/svg/inline', 'companies.svg' ); ?></svg>
+    <svg class="companies" width="340" height="100"><?php get_template_part( 'assets/svg/inline', 'companies.svg' ); ?></svg>
   </div>
 </div>
