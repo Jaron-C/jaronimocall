d2e6679fcf19bccc3a2ed7bb85783333c391f173
Jaron Call <jaroninthesis@gmail.com>
2016-03-08 22:38:11 -0700

    Added/deleted contact form/added footer

--- site/web/app/themes/sage/templates/footer.php
+++ site/web/app/themes/sage/templates/footer.php
@@ -3,3 +3,9 @@
     <?php dynamic_sidebar('sidebar-footer'); ?>
   </div>
 </footer>
+
+<div id="footer">
+  <div class="text-center">
+    <p>Made with love by <strong>Jaron Call</stong></p>
+  </div>
+</div>
