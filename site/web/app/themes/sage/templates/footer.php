eb481114e3a06507efd3f9401393a734d03a7483
Jaron Call <jaroninthesis@gmail.com>
2016-03-09 11:45:53 -0700

    Minimized footer.php

--- site/web/app/themes/sage/templates/footer.php
+++ site/web/app/themes/sage/templates/footer.php
@@ -1,11 +1,6 @@
 <footer class="content-info">
-  <div class="container">
+  <div class="container text-center" id="footer">
+    <p>Created with love by <strong>Jaron Call</stong></p>
     <?php dynamic_sidebar('sidebar-footer'); ?>
   </div>
 </footer>
-
-<div id="footer">
-  <div class="credit text-center">
-    <p>Created with love by <strong>Jaron Call</stong></p>
-  </div>
-</div>
