a2edafb1ef4d29a3a2c6948501861bc49c302520
Jaron Call <jaroninthesis@gmail.com>
2016-03-09 12:17:38 -0700

    Added about-list.svg to section1

--- site/web/app/themes/sage/templates/section1.php
+++ site/web/app/themes/sage/templates/section1.php
@@ -1,5 +1,11 @@
 <div id="section1">
   <div class="text-center">
+    <svg class="about-list" width="911" height="466"><?php get_template_part( 'assets/svg/inline', 'about-list.svg' ); ?></svg>
+  </div>
+</div>
+
+<div id="section1">
+  <div class="text-center">
     <svg class="companies" width="340" height="100"><?php get_template_part( 'assets/svg/inline', 'companies.svg' ); ?></svg>
   </div>
 </div>
