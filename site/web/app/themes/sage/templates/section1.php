216db4ba76ddf3bd23a7cf05569caae8b2edbe93
Jaron Call <jaroninthesis@gmail.com>
2016-03-11 12:27:02 -0700

    Fixed/added quote.svg

--- site/web/app/themes/sage/templates/section1.php
+++ site/web/app/themes/sage/templates/section1.php
@@ -4,6 +4,11 @@
   </div>
 </div>
 
+<div class="text-center">
+  <svg class="quote" width="236" height="78"><?php get_template_part( 'assets/svg/inline', 'quote.svg' ); ?></svg>
+</div>
+</div>
+
   <div class="text-center">
     <svg class="companies" width="334" height="78"><?php get_template_part( 'assets/svg/inline', 'companies.svg' ); ?></svg>
   </div>
