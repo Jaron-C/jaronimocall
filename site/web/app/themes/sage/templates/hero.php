c4d8bda2d021f4c88401323aaa2ad5ec67ffc5bb
Jaron Call <jaroninthesis@gmail.com>
2016-03-13 21:28:28 -0600

    Fixed h1 syntax

--- site/web/app/themes/sage/templates/hero.php
+++ site/web/app/themes/sage/templates/hero.php
@@ -2,8 +2,7 @@
   <div class="text-center">
     <svg class="hair-logo" width="421" height="526"><?php get_template_part( 'assets/svg/inline', 'hair-logo.svg' ); ?></svg>
   </div>
-</div>
-
-<div class="name text-center">
-  <h1>JARONIMO CALL</h1>
+  <div class="name text-center">
+    <h1>JARONIMO CALL</h1>
+  </div>
 </div>
--- site/web/app/themes/sage/templates/section1.php
+++ site/web/app/themes/sage/templates/section1.php
@@ -1,12 +1,12 @@
 <div id="section1">
-  <div class="text-center">
+  <div class="about-list text-center">
     <svg class="about-list" width="711" height="266"><?php get_template_part( 'assets/svg/inline', 'about-list.svg' ); ?></svg>
   </div>
 </div>
 
 
 
-  <div class="text-center">
+  <div class="companies text-center">
     <svg class="companies" width="334" height="78"><?php get_template_part( 'assets/svg/inline', 'companies.svg' ); ?></svg>
   </div>
 </div>
