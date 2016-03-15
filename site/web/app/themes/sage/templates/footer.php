a6860e4191da5ca5f70543c1d6f899da49bfdbe4
Jaron Call <jaroninthesis@gmail.com>
2016-03-15 16:29:12 -0600

    Changed sizing of logo/footer

--- site/web/app/themes/sage/templates/footer.php
+++ site/web/app/themes/sage/templates/footer.php
@@ -1,6 +1,6 @@
 <footer class="content-info">
   <div class="container text-center" id="footer">
-    <p>Created with love by <strong>Jaron Call</stong></p>
+    <p>Created by <strong>Jaron Call</stong></p>
     <?php dynamic_sidebar('sidebar-footer'); ?>
   </div>
 </footer>
