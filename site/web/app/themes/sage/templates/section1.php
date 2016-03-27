ddaa99c245e924071ffeeed95ac07db15ec5bce5
Jaron Call <jaroninthesis@gmail.com>
2016-03-27 05:50:42 -0600

    Styled quote button

--- site/web/app/themes/sage/templates/section1.php
+++ site/web/app/themes/sage/templates/section1.php
@@ -4,11 +4,13 @@
   </div>
 </div>
 
-<div class="CTA text-center">
-<button type="button" class="btn-default btn-lg center-block">Grab A Quote!</button>
+<div class="col-md-12 text-center">
+  <button id="singlebutton" name="singlebutton" class="btn btn-primary btn-lg">Grab A Quote!</button>
+  </div>
 </div>
 
-  <div class="companies">
+<div class="info text-center">
+  <div class="companies center-block">
     <svg class="companies" width="336" height="78"><?php get_template_part( 'assets/svg/inline', 'companies.svg' ); ?></svg>
   </div>
 </div>
