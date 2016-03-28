f9ffc152a5213ffacbf65977a35771b5ea04e31a
Jaron Call <jaroninthesis@gmail.com>
2016-03-28 12:42:25 -0600

    Cleaned up footer.php/scss

--- site/web/app/themes/sage/templates/footer.php
+++ site/web/app/themes/sage/templates/footer.php
@@ -1,7 +1,4 @@
-<footer class="content-info text-center">
   <div class="container text-center" id="footer navbar-fixed-bottom">
-    <a class="navbar-brand" href="<?= esc_url(home_url('/')); ?>">JC</a>
+    <a class="nav-footer" href="<?= esc_url(home_url('/')); ?>">JC</a>
   </div>
     <?php dynamic_sidebar('sidebar-footer'); ?>
-  </div>
-</footer>
