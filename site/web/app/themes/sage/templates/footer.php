3b3ef68a39e2bd0b548d57ef889c630bca1311b7
Jaron Call <jaroninthesis@gmail.com>
2016-03-28 13:52:03 -0600

    Started working contact-page.php

--- site/web/app/themes/sage/templates/footer.php
+++ site/web/app/themes/sage/templates/footer.php
@@ -1,4 +1,4 @@
-  <div class="container text-center" id="footer navbar-fixed-bottom">
+  <div class="footer text-center" id="footer navbar-fixed-bottom">
     <a class="nav-footer" href="<?= esc_url(home_url('/')); ?>">JC</a>
   </div>
     <?php dynamic_sidebar('sidebar-footer'); ?>
