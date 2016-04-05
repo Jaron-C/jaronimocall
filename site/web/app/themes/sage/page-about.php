ee3200fc267a2adacd096fed6a43d3df75097e74
Jaron Call <jaroninthesis@gmail.com>
2016-04-04 21:33:17 -0600

    Added svg to about page

--- site/web/app/themes/sage/page-about.php
+++ site/web/app/themes/sage/page-about.php
@@ -2,3 +2,9 @@
   <?php get_template_part('templates/page', 'header'); ?>
   <?php get_template_part('templates/content', 'page'); ?>
 <?php endwhile; ?>
+
+<div id="rundown">
+  <div class="about-rundown text-center">
+    <svg class="about-rundown img-responsive" width="605" height="630"><?php get_template_part( 'assets/svg/inline', 'about-rundown.svg' ); ?></svg>
+  </div>
+</div>
