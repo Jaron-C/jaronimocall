779464786d488b1cad085aeb1853519f93bdc475
Jaron Call <jaroninthesis@gmail.com>
2016-04-06 23:24:02 -0600

    Organized _pages.scss and .php files

--- site/web/app/themes/sage/page-about.php
+++ site/web/app/themes/sage/page-about.php
@@ -5,6 +5,7 @@
 
 <!-- Place "personal.jpg" Here -->
 
+<!-- Weclome  Text -->
 <div class="about-copy text-center">
   <p style="text-align: center;font-size: 25px;">Welcome to <strong>jaronimocall.com</strong></p>
   &nbsp;
@@ -15,17 +16,16 @@
     <div>Along side my design, I also run a local Utah Organization called <strong>Brake The Cycle</strong>,</div>
     <div>which is in support of suicide awareness.</div>
   </div>
+</div>
   &nbsp;
-  <div style="text-align: center;">Listed below is a quick rundown of what you can expect from this website!</div>
+<div class="about-copy-2">
+  <div style="text-align: center;">Listed below is a quick rundown</div>
+  <div style="text-align: center;">of what you can expect from this website!</div>
   <div style="text-align: center;"></div>
-  &nbsp;
-
 </div>
   &nbsp;
 
-
-<div id="rundown">
-  <div class="about-rundown text-center">
+<!-- about-rundown svg -->
+<div class="about-rundown text-center">
     <svg class="about-rundown img-responsive" width="705" height="726"><?php get_template_part( 'assets/svg/inline', 'about-rundown.svg' ); ?></svg>
-  </div>
 </div>
