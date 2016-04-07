c34d72c02b505c70aa7780fc26561351d0298603
Jaron Call <jaroninthesis@gmail.com>
2016-04-07 16:38:28 -0600

    Added workflow wistia video

--- site/web/app/themes/sage/page-projects.php
+++ site/web/app/themes/sage/page-projects.php
@@ -3,19 +3,28 @@
   <?php get_template_part('templates/content', 'page'); ?>
 <?php endwhile; ?>-->
 
-<div id="videos">
-  <div class="category-1 text-center">
-    <h3>Motion Graphics</h3>
-    <iframe src="//fast.wistia.net/embed/playlists/zl5tr460h0?media_0_0%5BautoPlay%5D=false&media_0_0%5BcontrolsVisibleOnLoad%5D=true&plugin%5Bsocialbar-v1%5D%5Bbuttons%5D=twitter-linkedIn-facebook&theme=tab&version=v1&videoOptions%5BautoPlay%5D=true&videoOptions%5BplayerColor%5D=6da2a3&videoOptions%5BvideoHeight%5D=360&videoOptions%5BvideoWidth%5D=640&videoOptions%5BvolumeControl%5D=true" allowtransparency="true" frameborder="0" scrolling="no" class="wistia_playlist" name="wistia_playlist" allowfullscreen mozallowfullscreen webkitallowfullscreen oallowfullscreen msallowfullscreen width="640" height="386"></iframe>
+<div class="row">
+  <div class="col-md-6">
+    <div class="category-1 text-center">
+      <h3>Motion Graphics</h3>
+      <iframe src="//fast.wistia.net/embed/playlists/zl5tr460h0?media_0_0%5BautoPlay%5D=false&media_0_0%5BcontrolsVisibleOnLoad%5D=true&plugin%5Bsocialbar-v1%5D%5Bbuttons%5D=twitter-linkedIn-facebook&theme=tab&version=v1&videoOptions%5BautoPlay%5D=true&videoOptions%5BplayerColor%5D=6da2a3&videoOptions%5BvideoHeight%5D=360&videoOptions%5BvideoWidth%5D=640&videoOptions%5BvolumeControl%5D=true" allowtransparency="true" frameborder="0" scrolling="no" class="wistia_playlist" name="wistia_playlist" allowfullscreen mozallowfullscreen webkitallowfullscreen oallowfullscreen msallowfullscreen width="340" height="186"></iframe>
+    </div>
+
+    <div class="category-2 text-center">
+      <h3>Workflow</h3>
+      <iframe src="//fast.wistia.net/embed/playlists/fygsxd1srx?media_0_0%5BautoPlay%5D=false&media_0_0%5BcontrolsVisibleOnLoad%5D=true&theme=tab&version=v1&videoOptions%5BautoPlay%5D=true&videoOptions%5BplayerColor%5D=6da2a3&videoOptions%5BvideoHeight%5D=373&videoOptions%5BvideoWidth%5D=663&videoOptions%5BvolumeControl%5D=true" allowtransparency="true" frameborder="0" scrolling="no" class="wistia_playlist" name="wistia_playlist" allowfullscreen mozallowfullscreen webkitallowfullscreen oallowfullscreen msallowfullscreen width="340" height="186"></iframe>
+    </div>
   </div>
 
-  <div class="category-2 text-center">
-    <h3>iPhone Jams</h3>
-    <iframe src="//fast.wistia.net/embed/playlists/xb0cjz615s?media_0_0%5BautoPlay%5D=false&media_0_0%5BcontrolsVisibleOnLoad%5D=true&plugin%5Bsocialbar-v1%5D%5Bbuttons%5D=twitter-linkedIn-facebook&theme=tab&version=v1&videoOptions%5BautoPlay%5D=true&videoOptions%5BvideoHeight%5D=360&videoOptions%5BvideoWidth%5D=640&videoOptions%5BvolumeControl%5D=true" allowtransparency="true" frameborder="0" scrolling="no" class="wistia_playlist" name="wistia_playlist" allowfullscreen mozallowfullscreen webkitallowfullscreen oallowfullscreen msallowfullscreen width="640" height="386"></iframe>
-  </div>
-
+  <div class="col-md-6">
   <div class="category-3 text-center">
     <h3>Insta Edits</h3>
-    <iframe src="//fast.wistia.net/embed/playlists/ygrf4ch2z2?media_0_0%5BautoPlay%5D=false&media_0_0%5BcontrolsVisibleOnLoad%5D=true&plugin%5Bsocialbar-v1%5D%5Bbuttons%5D=twitter-linkedIn-facebook&theme=tab&version=v1&videoOptions%5BautoPlay%5D=true&videoOptions%5BplayerColor%5D=6da2a3&videoOptions%5BvideoHeight%5D=360&videoOptions%5BvideoWidth%5D=640&videoOptions%5BvolumeControl%5D=true" allowtransparency="true" frameborder="0" scrolling="no" class="wistia_playlist" name="wistia_playlist" allowfullscreen mozallowfullscreen webkitallowfullscreen oallowfullscreen msallowfullscreen width="640" height="386"></iframe>
+    <iframe src="//fast.wistia.net/embed/playlists/ygrf4ch2z2?media_0_0%5BautoPlay%5D=false&media_0_0%5BcontrolsVisibleOnLoad%5D=true&plugin%5Bsocialbar-v1%5D%5Bbuttons%5D=twitter-linkedIn-facebook&theme=tab&version=v1&videoOptions%5BautoPlay%5D=true&videoOptions%5BplayerColor%5D=6da2a3&videoOptions%5BvideoHeight%5D=360&videoOptions%5BvideoWidth%5D=640&videoOptions%5BvolumeControl%5D=true" allowtransparency="true" frameborder="0" scrolling="no" class="wistia_playlist" name="wistia_playlist" allowfullscreen mozallowfullscreen webkitallowfullscreen oallowfullscreen msallowfullscreen width="340" height="186"></iframe>
   </div>
+  <div class="category 4">
+    <h3>iPhone Jams</h3>
+    <iframe src="//fast.wistia.net/embed/playlists/xb0cjz615s?media_0_0%5BautoPlay%5D=false&media_0_0%5BcontrolsVisibleOnLoad%5D=true&plugin%5Bsocialbar-v1%5D%5Bbuttons%5D=twitter-linkedIn-facebook&theme=tab&version=v1&videoOptions%5BautoPlay%5D=true&videoOptions%5BvideoHeight%5D=360&videoOptions%5BvideoWidth%5D=640&videoOptions%5BvolumeControl%5D=true" allowtransparency="true" frameborder="0" scrolling="no" class="wistia_playlist" name="wistia_playlist" allowfullscreen mozallowfullscreen webkitallowfullscreen oallowfullscreen msallowfullscreen width="340" height="186"></iframe>
+  </div>
+</div>
+</div>
 </div>
