ae83c064a26a7d77f1effc2cc5c38e16befc22e9
Jaron Call <jaroninthesis@gmail.com>
2016-04-09 11:53:46 -0600

    Fixed inquiry text on about page

--- site/web/app/themes/sage/page-about.php
+++ site/web/app/themes/sage/page-about.php
@@ -43,4 +43,4 @@
 </div>
 </div>
 
-<div style="text-align: center;" class="about-inquire"><h4>For project inquiries, please fill out form on Contact Page</h4></div>
+<div style="text-align: center;" class="about-inquire"><h4>For project inquiries, please fill out the form on the Contact Page</h4></div>
