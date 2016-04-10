89ecf8160002be17ec548b0df14a0574a6c4f51d
Jaron Call <jaroninthesis@gmail.com>
2016-04-10 12:45:58 -0600

    Removed row|column from about page

--- site/web/app/themes/sage/page-about.php
+++ site/web/app/themes/sage/page-about.php
@@ -4,8 +4,7 @@
 <?php endwhile; ?>-->
 
 <!-- personal.jpg -->
-<div class="row text-center">
-<div class="col-md-6 text-center">
+<div class="container text-center">
   <div class="photo">
     <img src="<?=get_stylesheet_directory_uri();?>/dist/images/personal.jpg">
   </div>
@@ -18,18 +17,17 @@
 </div>
 
 
-<!-- Top Text -->
-<div class="col-md-6">
-    <div class="about-copy text-center">
-        <div style="text-align: center;"><strong>Motion Graphics</strong> and <strong>Website Design.</strong></div>
-        <div style="text-align: center;">Creative for <strong>Jackalope Media</strong>.</div>
-        <div style="text-align: center;">
-        <div>Co-Founder of local Utah Organization <strong>Brake The Cycle</strong>,</div>
-        <div>which is in support of suicide prevention.</div>
-      </div>
-    </div>
+<!-- Top Text-->
+
+<div class="about-copy text-center">
+  <div style="text-align: center;"><strong>Motion Graphics</strong> and <strong>Website Design.</strong></div>
+  <div style="text-align: center;">Creative for <strong>Jackalope Media</strong>.</div>
+  <div style="text-align: center;">
+   <div>Co-Founder of local Utah Organization <strong>Brake The Cycle</strong>,</div>
+   <div>which is in support of suicide prevention.</div>
   </div>
 </div>
+
 <!-- Bottom Text -->
 <div class="about-copy-2">
   <div style="text-align: center;">Below is a quick rundown of what</div>
