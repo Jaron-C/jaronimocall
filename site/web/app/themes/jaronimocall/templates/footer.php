3ddda3c76e510cff8f42ac727ccb7bc95a7e3596
Jaron Call <jaroninthesis@gmail.com>
2016-05-21 00:02:27 -0600

    Vagrant Up jackalope.io-v2.0 & attempted gulp/gulp watch
    Vagrant Up, Gulp and Gulp Watch.
    *Ran into error with gulp and gulp watch when running on Sage Theme*

--- site/web/app/themes/jaronimocall/templates/footer.php
+++
@@ -1,7 +0,0 @@
-  <div class="footer text-center" id="footer navbar-fixed-bottom">
-    <a class="nav-footer" href="<?= esc_url(home_url('/')); ?>">JC</a>
-  </div>
-    <?php dynamic_sidebar('sidebar-footer'); ?>
-<div class="footer-creator">
-  <p style="text-align: center;">©<em>Created with love by </em><strong>Jaron Call</strong></p>
-</div>
